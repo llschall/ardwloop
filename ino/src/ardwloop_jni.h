e16bdf0926d8dd1139f0bd115adc5cc74f85ce96
llschall <llschall@gmx.com>
2024-02-20 00:25:40 +0100

    [Ino] Move implementation from header to cpp file

--- ino/src/ardwloop_jni.h
+++ ino/src/ardwloop_jni.h
@@ -1,43 +1,22 @@
 #ifndef ardwloop_jni
 #define ardwloop_jni
 
-#include <stdio.h>
 #include "ardwloop_core.h"
 
-int entry_check(int i) {
-  printf("\n# ardwloop_jni.h entry_check# %d\n", i);
-  return i * 2;
-}
+int entry_check(int i);
 
-void entry_begin(int reboot, int read, int post, int beforeK, int j) {
-  ardw_begin(reboot, read, post, beforeK, j);
-}
+void entry_begin(int reboot, int read, int post, int beforeK, int j);
 
-void entry_setup() {
-  printf("\n# ardwloop_jni.h entry_setup#\n");
-  ardw_setup();
-}
+void entry_setup();
 
-void entry_loop() {
-  printf("\n# ardwloop_jni.h entry_loop#\n");
-  ardw_loop();
-}
+void entry_loop();
 
-void entry_reset() {
-  //back_print(1, "# ardwloop_jni.h entry_reset#");
-  //reset();
-}
+void entry_reset();
 
-char entry_prg() {
-  return ardw_prg();
-}
+char entry_prg();
 
-V* entry_s() {
-  return ardw_s();
-}
+V* entry_s();
 
-V* entry_r() {
-  return ardw_r();
-}
+V* entry_r();
 
 #endif
