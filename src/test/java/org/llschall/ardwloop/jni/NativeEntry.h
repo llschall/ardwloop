29a0bbf33f4a70b7b32b2797bfaa1c547b776b6f
llschall <llschall@gmx.com>
2024-02-26 12:16:23 +0100

    [Jni] Format code

--- src/test/java/org/llschall/ardwloop/jni/NativeEntry.h
+++ src/test/java/org/llschall/ardwloop/jni/NativeEntry.h
@@ -1,14 +1,14 @@
 
-      void fake_delay(unsigned long);
-      void fake_write_low(int);
-      void fake_write_high(int);
-      void fake_pin_out(int);
-      void fake_serial_begin(int);
-      int fake_available();
-      int fake_read(int);
-      int fake_write(char);
+    void fake_delay(unsigned long);
+    void fake_write_low(int);
+    void fake_write_high(int);
+    void fake_pin_out(int);
+    void fake_serial_begin(int);
+    int fake_available();
+    int fake_read(int);
+    int fake_write(char);
 
-      void back_print(int log, char* str);
-      void back_print(int log, char* str, va_list c);
+    void back_print(int log, char* str);
+    void back_print(int log, char* str, va_list c);
 
-      jclass findBackEntryClass();
+    jclass findBackEntryClass();
