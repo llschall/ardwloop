22bfe6301dd38bd74f5caf07a6136ebc6fcd08c9
llschall <llschall@gmx.com>
2024-11-24 19:37:05 +0100

    [Snapshot] Switch to 0.2.1

--- examples/demo_led/demo_led.ino
+++ examples/demo_led/demo_led.ino
@@ -1,7 +1,7 @@
 /*
  * Welcome to the Ardwloop Demo !
  *
- * Featuring Ardwloop 0.2.0
+ * Featuring Ardwloop 0.2.1
  *
  * More setup instructions are available in
  * https://llschall.github.io/ardwloop/demo
