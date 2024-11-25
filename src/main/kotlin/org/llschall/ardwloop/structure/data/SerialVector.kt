34243d7860564a103839d83439055c1b9a1143cd
llschall <llschall@gmx.com>
2024-11-25 21:09:51 +0100

    [Vector] Make values non-nullable and writable

--- src/main/kotlin/org/llschall/ardwloop/structure/data/SerialVector.kt
+++ src/main/kotlin/org/llschall/ardwloop/structure/data/SerialVector.kt
@@ -1,27 +1,13 @@
 package org.llschall.ardwloop.structure.data
 
 class SerialVector @JvmOverloads constructor(
-    v: Int? = null,
-    w: Int? = null,
-    x: Int? = null,
-    y: Int? = null,
-    z: Int? = null
+    @JvmField val v: Int = 0,
+    @JvmField val w: Int = 0,
+    @JvmField val x: Int = 0,
+    @JvmField val y: Int = 0,
+    @JvmField val z: Int = 0,
 ) {
-    @JvmField
-    val v: Int? = v
-
-    @JvmField
-    val w: Int? = w
-
-    @JvmField
-    val x: Int? = x
-
-    @JvmField
-    val y: Int? = y
-
-    @JvmField
-    val z: Int? = z
-
+    
     override fun toString(): String {
         return "" +
                 "v" + v +
