e7ca9e3907666b15ad77e64c61df8773b087b717
llschall <llschall@gmx.com>
2024-12-01 14:45:08 +0100

    [Test] Fix Kotlin test

--- src/test/kotlin/org/llschall/ardwloop/ArdwloopKTest.kt
+++ src/test/kotlin/org/llschall/ardwloop/ArdwloopKTest.kt
@@ -2,13 +2,16 @@ package org.llschall.ardwloop
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.llschall.ardwloop.value.V
+import org.llschall.ardwloop.value.ValueMap
 
 class ArdwloopKTest {
 
     @Test
     fun testProgramCanBeCreated() {
         val program = JTestProgram()
-        val rc = program.rc
-        Assertions.assertEquals(1, rc)
+
+        val map = program.ardwSetup(ValueMap())
+        Assertions.assertEquals(1, map.a[V.v])
     }
 }
