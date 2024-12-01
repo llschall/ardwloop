b1f2a0b7ad2170bf5219fe80652e1d8f071aca2d
llschall <llschall@gmx.com>
2024-12-01 15:24:48 +0100

    [Snapshot] Switch to 0.2.4

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ repositories {
 }
 
 dependencies {
-    implementation 'io.github.llschall:ardwloop:0.2.3'
+    implementation 'io.github.llschall:ardwloop:0.2.4'
 }
 ```
 
--- ino/examples/demo_led/demo_led.ino
+++ ino/examples/demo_led/demo_led.ino
@@ -1,7 +1,7 @@
 /*
  * Welcome to the Ardwloop Demo !
  *
- * Featuring Ardwloop 0.2.3
+ * Featuring Ardwloop 0.2.4
  *
  * More setup instructions are available in
  * https://llschall.github.io/ardwloop/demo
