8fbe26fd377c651310f64939b09b1db84e2a8e4a
llschall <llschall@gmx.com>
2024-12-27 17:02:05 +0100

    [API] Expose add() method

--- src/main/kotlin/org/llschall/ardwloop/structure/model/EventQueue.kt
+++ src/main/kotlin/org/llschall/ardwloop/structure/model/EventQueue.kt
@@ -5,6 +5,10 @@ import java.util.concurrent.ConcurrentLinkedQueue
 class EventQueue {
     private val events = ConcurrentLinkedQueue<Event>()
 
+    fun add(event: Event) {
+        events.add(event)
+    }
+
     fun next(): Event? {
         return events.poll()
     }
