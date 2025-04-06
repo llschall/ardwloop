297784067de536e9ef082dea3c0a44ec4a2b7ae0
llschall <llschall@gmx.com>
2025-04-06 19:37:34 +0200

    [Release] Publish 0.3.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,7 +54,7 @@ if (!project.hasProperty("token_usr")) {
 
                     groupId = "io.github.llschall"
                     artifactId = "ardwloop"
-                    version = "0.3.3-SNAPSHOT"
+                    version = "0.3.3"
 
                     pom {
                         signing {
@@ -90,7 +90,7 @@ if (!project.hasProperty("token_usr")) {
                         username = user
                         password = pwd
                     }
-                    url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots")
+                    url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2")
                 }
             }
             signing {
