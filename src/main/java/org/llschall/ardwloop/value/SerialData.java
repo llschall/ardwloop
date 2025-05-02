16815a615a97e8983de6c0d46d68a05b7d31252f
llschall <llschall@gmx.com>
2025-05-02 12:25:56 +0200

    [Doc] Add comments

--- src/main/java/org/llschall/ardwloop/value/SerialData.java
+++ src/main/java/org/llschall/ardwloop/value/SerialData.java
@@ -2,20 +2,55 @@ package org.llschall.ardwloop.value;
 
 import org.llschall.ardwloop.structure.StructureException;
 
+/**
+ * The class SerialData is used to store the data that is sent and received from the Arduino.
+ * It wraps the data as vectors of 5 integers.
+ */
 public class SerialData {
 
     public String str = "";
 
+    /**
+     * The vector a that wraps 5 integers
+     */
     public final V a = new V();
+    /**
+     * The vector b that wraps 5 integers
+     */
     public final V b = new V();
+
+    /**
+     * The vector c that wraps 5 integers
+     */
     public final V c = new V();
+    /**
+     * The vector d that wraps 5 integers
+     */
     public final V d = new V();
+    /**
+     * The vector e that wraps 5 integers
+     */
     public final V e = new V();
+    /**
+     * The vector f that wraps 5 integers
+     */
     public final V f = new V();
+    /**
+     * The vector g that wraps 5 integers
+     */
     public final V g = new V();
+    /**
+     * The vector h that wraps 5 integers
+     */
     public final V h = new V();
+    /**
+     * The vector i that wraps 5 integers
+     */
     public final V i = new V();
 
+    /**
+     * Constructor that creates an empty SerialData
+     */
     public SerialData() {
     }
 
