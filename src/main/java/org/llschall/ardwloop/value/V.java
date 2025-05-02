dde084adf77151c86e3b78e00616c9b709cd62a4
llschall <llschall@gmx.com>
2025-05-02 21:42:17 +0200

    [Doc] Add comments

--- src/main/java/org/llschall/ardwloop/value/V.java
+++ src/main/java/org/llschall/ardwloop/value/V.java
@@ -2,21 +2,43 @@ package org.llschall.ardwloop.value;
 
 import org.llschall.ardwloop.structure.StructureException;
 
+/**
+ * The V class represents a structure with five integer values: v, w, x, y, and z.
+ */
 public class V {
 
+    /**
+     * The integer value of v.
+     */
     public int v;
+
+    /**
+     * The integer value of w.
+     */
     public int w;
+
+    /**
+     * The integer value of x.
+     */
     public int x;
+
+    /**
+     * The integer value of y.
+     */
     public int y;
+
+    /**
+     * The integer value of z.
+     */
     public int z;
 
     public void fromChar(char c, int i) {
         switch (c) {
-            case 'v' -> v=i;
-            case 'w' -> w=i;
-            case 'x' -> x=i;
-            case 'y' -> y=i;
-            case 'z' -> z=i;
+            case 'v' -> v = i;
+            case 'w' -> w = i;
+            case 'x' -> x = i;
+            case 'y' -> y = i;
+            case 'z' -> z = i;
             default -> throw new StructureException("Unexpected character: " + c);
         }
     }
