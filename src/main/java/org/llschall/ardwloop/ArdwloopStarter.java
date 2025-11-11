d603dbc4fb53023a699090cf25aa0223de85bfdf
llschall <llschall@gmx.com>
2025-11-11 08:32:32 +0100

    [Build] Bump to version 0.3.7

--- src/main/java/org/llschall/ardwloop/ArdwloopStarter.java
+++ src/main/java/org/llschall/ardwloop/ArdwloopStarter.java
@@ -23,7 +23,7 @@ public class ArdwloopStarter {
     /**
      * The version of the Ardwloop library
      */
-    public static final String VERSION = "0.3.6";
+    public static final String VERSION = "0.3.7";
 
     /**
      * An integer that might vary with the snapshot
