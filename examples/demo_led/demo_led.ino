378c6e982a5664b15a8fa5e1a56f4058ba1194d0
llschall <llschall@gmx.com>
2025-12-08 21:33:06 +0100

    [Build] Bump to version 0.3.8

--- examples/demo_led/demo_led.ino
+++ examples/demo_led/demo_led.ino
@@ -1,7 +1,7 @@
 /*
  * Welcome to the Ardwloop Demo !
  *
- * Featuring Ardwloop 0.3.7
+ * Featuring Ardwloop 0.3.8
  *
  * More setup instructions are available in
  * https://llschall.github.io/ardwloop/demo
